515a6a31b9a283b3ab54df2684be3e214f3ef7cf
coffm1n <coffm1n@cmich.edu>
2026-09-21 15:02:23 -0400

    Load tasks from text file

--- tasktrack.py
+++ tasktrack.py
@@ -4,6 +4,7 @@ Author: Nicolas Coffman
 Course: CPS 310
 """
 
+TASKS_FILE = "tasks.txt"
 
 def display_menu():
     """Display the available TaskTrack menu options."""
@@ -31,10 +32,30 @@ def view_tasks(tasks):
     for number, task in enumerate(tasks, start=1):
         print(f"{number}. {task}")
 
+def load_tasks(filename):
+    """Load tasks from a text file and return them as a list"""
+    tasks = []
+
+    try:
+        with open(filename, "r") as file:
+            for line in file:
+                task_clean = line.strip()
+                
+                #TODO: ignore blank lines
+                if not task_clean: #evaulates to true if the line is blank, doesnt get added to tasks
+                    continue 
+
+                #TODO: add each non-empty task to the tasks list
+                tasks.append(task_clean)
+                
+    except FileNotFoundError:
+        # A new project may not have a task file yet
+        return []
+    return tasks
 
 def main():
     """Run the TaskTrack menu until the user chooses to exit."""
-    tasks = []
+    tasks = load_tasks(TASKS_FILE)
 
     while True:
         display_menu()
